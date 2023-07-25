21400ff534a05adbaeebbb72871156d2a45f5467
unknown <sewardchard@gmail.com>
2023-07-25 17:30:12 +0300

    Code adjustments

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BabaRiri/puppy
 
 go 1.20
+
+require github.com/BabaRiri/dog v0.0.0-20230725140039-3481b4f319b3 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/BabaRiri/dog"
+)
+
 func Bark() string {
 	return "🐶Woof!"
 }
 
 func Barks() string {
 	return "🐶Woof! 🐶Woof! 🐶Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
